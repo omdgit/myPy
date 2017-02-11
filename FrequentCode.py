e0130b69938e09f8b8b136c9b3b12f56bcfc605c
omdgit <oliver.dreger@gmail.com>
2017-02-11 15:02:11 -0500

    code that I frequently use

---
+++ FrequentCode.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 11 14:45:40 2017
+
+@author: omdgit
+"""
+
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+print("Python version: %s" % sys.version)
+print("Pandas version: %s" % pd.__version__)
+print("Numpy version: %s" % np.__version__)
+print("Matplotlib version: %s" % matplotlib.__version__)
+
+# proc format equivalent
